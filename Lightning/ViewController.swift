571babb197a09a7aa24d95b351c3032d056da4ab
Matteo <immatteoz@gmail.com>
2024-02-01 13:01:16 +0800

    Initial Commit

---
+++ Lightning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lightning
+//
+//  Created by nicola on 2024/2/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
